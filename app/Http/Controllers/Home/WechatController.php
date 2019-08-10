5857a4cb16646c8ce834dad2c021fb3b29841c1d
xuzhaowen <yangzeloveyou@gmail.com>
2019-08-10 16:52:48 +0800

    线上测试

--- app/Http/Controllers/Home/WechatController.php
+++ app/Http/Controllers/Home/WechatController.php
@@ -15,32 +15,33 @@ class WechatController extends Controller
 
     public function wechat_web_login()
     {
-        if(Auth::check()){
+        if (Auth::check()) {
             return redirect('/home');
         }
         return view('wechat_web_login');
     }
+
     public function api_web_login()
     {
         $code = request()->code;
 
 
-        if((!$code)||!Cache::has($code)){
+        if ((!$code) || !Cache::has($code)) {
             $code = uniqid();
-            $filename =$code.'.png';
-            Cache::put($code,'login',10);
-            $qrCode = new QrCode("http://wechat-scan-login.jc91715.top/wechat/{$code}/login");
-            \Log::info(storage_path('app/public/'.$filename));
-            $qrCode->writeFile(storage_path('app/public/'.$filename));
+            $filename = $code . '.png';
+            Cache::put($code, 'login', 10);
+            $qrCode = new QrCode(env('APP_URL') . "/wechat/{$code}/login");
+            \Log::info(storage_path('app/public/' . $filename));
+            $qrCode->writeFile(storage_path('app/public/' . $filename));
         }
-        $filename =$code.'.png';
-        $msg ='请扫码登录';
-        $errorCode=1;
-        if(Cache::has($code.'login_state')){
-            $login_state = Cache::get($code.'login_state');
-            switch ($login_state){
+        $filename = $code . '.png';
+        $msg = '请扫码登录';
+        $errorCode = 1;
+        if (Cache::has($code . 'login_state')) {
+            $login_state = Cache::get($code . 'login_state');
+            switch ($login_state) {
                 case 'scan':
-                    $errorCode =1;
+                    $errorCode = 1;
                     $msg = '已扫描';
                     break;
                 case 'cancel':
@@ -48,73 +49,75 @@ class WechatController extends Controller
                     $msg = '已取消';
                     break;
                 case 'confirm':
-                    $errorCode=0;
+                    $errorCode = 0;
                     $msg = '已确认登录';
                     break;
             }
         }
-        return json_encode(['errorCode'=>$errorCode,'code'=>$code,'msg'=>$msg,'qrcode_url'=>asset('storage/'.$filename),'redirect'=>route('web.login').'?code='.$code]);
+        return json_encode(['errorCode' => $errorCode, 'code' => $code, 'msg' => $msg, 'qrcode_url' => asset('storage/' . $filename), 'redirect' => route('web.login') . '?code=' . $code]);
     }
+
     public function web_login()
     {
         $code = request()->code;
-        if(!Cache::has($code)){
+        if (!Cache::has($code)) {
             abort(404);
         }
-        if(!Cache::has($code.'login_state')){
+        if (!Cache::has($code . 'login_state')) {
             abort(404);
         }
-        if(Cache::get($code.'login_state')!='confirm'){
+        if (Cache::get($code . 'login_state') != 'confirm') {
             abort(404);
         }
-        $user = User::where('rand_key',$code)->first();
-        if(!$user){
-            abort(404,'code 失效');
+        $user = User::where('rand_key', $code)->first();
+        if (!$user) {
+            abort(404, 'code 失效');
         }
         Cache::forget($code);
         Auth::login($user);
         return redirect()->to(route('home'));
     }
 
-   public function login_code_state($code)
-   {
-       if (!Cache::has($code)) {
-           return json_encode(['errorCode'=>1,'msg'=>'登陆已过期']);
-       }
-       Cache::put($code.'login_state','scan',10);
-       return json_encode(['errorCode'=>0,'msg'=>'ok']);
-
-   }
-
-   public function confirm_login()
-   {
-        $code = request()->input('code');
-        if(!$code){
-            return json_encode(['errorCode'=>1,'msg'=>'缺少参数']);
+    public function login_code_state($code)
+    {
+        if (!Cache::has($code)) {
+            return json_encode(['errorCode' => 1, 'msg' => '登陆已过期']);
         }
-       if (!Cache::has($code)) {
-           return json_encode(['errorCode'=>1,'msg'=>'登陆已过期']);
-       }
+        Cache::put($code . 'login_state', 'scan', 10);
+        return json_encode(['errorCode' => 0, 'msg' => 'ok']);
+
+    }
+
+    public function confirm_login()
+    {
+        $code = request()->input('code');
+        if (!$code) {
+            return json_encode(['errorCode' => 1, 'msg' => '缺少参数']);
+        }
+        if (!Cache::has($code)) {
+            return json_encode(['errorCode' => 1, 'msg' => '登陆已过期']);
+        }
         $user = Auth::user();
         $user->rand_key = $code;
         $user->save();
-        Cache::put($code.'login_state','confirm',10);
+        Cache::put($code . 'login_state', 'confirm', 10);
 
-       return json_encode(['errorCode'=>0,'msg'=>'已确认登陆']);
+        return json_encode(['errorCode' => 0, 'msg' => '已确认登陆']);
 
 
-   }
-   public function cancel_login()
-   {
-       $code = request()->input('code');
-       if(!$code){
-           return json_encode(['errorCode'=>1,'msg'=>'缺少参数']);
-       }
-       if (!Cache::has($code)) {
-           return json_encode(['errorCode'=>1,'msg'=>'登陆已过期']);
-       }
-       Cache::put($code.'login_state','cancel',10);
+    }
 
-       return json_encode(['errorCode'=>0,'msg'=>'已取消登陆']);
-   }
+    public function cancel_login()
+    {
+        $code = request()->input('code');
+        if (!$code) {
+            return json_encode(['errorCode' => 1, 'msg' => '缺少参数']);
+        }
+        if (!Cache::has($code)) {
+            return json_encode(['errorCode' => 1, 'msg' => '登陆已过期']);
+        }
+        Cache::put($code . 'login_state', 'cancel', 10);
+
+        return json_encode(['errorCode' => 0, 'msg' => '已取消登陆']);
+    }
 }
